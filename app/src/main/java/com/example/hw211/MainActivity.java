6f84ca61739a82b3ddd55f3e2cd61e5c816ca5e3
allexcross <66046919+allexcross@users.noreply.github.com>
2020-08-21 11:25:27 +0300

    Initial commit

---
+++ app/src/main/java/com/example/hw211/MainActivity.java
@@ -0,0 +1,86 @@
+package com.example.hw211;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText mInputMoney;
+    private EditText mInputInfo;
+    private Button mBtnOk;
+    private CheckBox mBankCardChkBx;
+    private CheckBox mMobilePhoneChkBx;
+    private CheckBox mCashAddressChkBx;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mInputMoney = findViewById(R.id.inputMoney);
+        mInputInfo = findViewById(R.id.inputInfo);
+        mBtnOk = findViewById(R.id.btnOK);
+        mBankCardChkBx = findViewById(R.id.bankCardChkBx);
+        mMobilePhoneChkBx = findViewById(R.id.mobilePhoneChkBx);
+        mCashAddressChkBx = findViewById(R.id.cashAddressChkBx);
+
+        mBankCardChkBx.setOnCheckedChangeListener(checkedChangeListener);
+        mMobilePhoneChkBx.setOnCheckedChangeListener(checkedChangeListener);
+        mCashAddressChkBx.setOnCheckedChangeListener(checkedChangeListener);
+
+        mBankCardChkBx.setChecked(true);
+        mInputInfo.setInputType(InputType.TYPE_CLASS_NUMBER);
+
+
+        mBtnOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mInputMoney.getText().toString().isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Введите сумму!", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(MainActivity.this, "Оплачено!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+    }
+
+
+    CompoundButton.OnCheckedChangeListener checkedChangeListener = new CompoundButton.OnCheckedChangeListener() {
+        @Override
+        public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+            if (isChecked) {
+                switch (compoundButton.getId()) {
+                    case R.id.bankCardChkBx:
+                        mMobilePhoneChkBx.setChecked(false);
+                        mCashAddressChkBx.setChecked(false);
+                        mInputInfo.setInputType(InputType.TYPE_CLASS_NUMBER);
+                        break;
+                    case R.id.mobilePhoneChkBx:
+                        mBankCardChkBx.setChecked(false);
+                        mCashAddressChkBx.setChecked(false);
+                        mInputInfo.setInputType(InputType.TYPE_CLASS_PHONE);
+                        break;
+                    case R.id.cashAddressChkBx:
+                        mBankCardChkBx.setChecked(false);
+                        mMobilePhoneChkBx.setChecked(false);
+                        mInputInfo.setInputType(InputType.TYPE_CLASS_TEXT);
+                        break;
+                    default:
+                }
+            }
+        }
+    };
+
+
+}
